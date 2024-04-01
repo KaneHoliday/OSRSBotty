d3b78bcdbd829cf44a41cc1840f40b819cef2caf
KaneHoliday <163705985+KaneHoliday@users.noreply.github.com>
2024-04-01 18:27:08 +1100

    Update Colosseum.cs
    Start of colosseum script

---
+++ Corp/Scripts/Colosseum.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corp.Scripts
+{
+    internal class Colosseum
+    {
+        public Player player = new Player();
+        Peripherals peripherals = new Peripherals();
+        Interfaces interfaces = new Interfaces();
+
+        public int[] gameScreenCoords = new int[2] { 0, 0 };
+
+
+		public void analyzeWave() {
+		
+		
+
+		}
+
+        public void startScript()
+        {
+            
+        }
+    
+        public bool noBars() //change to something else
+        {
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            using (Bitmap bitmap = new Bitmap(30, 30))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(gameScreenCoords[0] + 557, gameScreenCoords[1] + 428, 0, 0, new Size(30, 30));
+                }
+
+                for (int i = 0; i < 30; i++)
+                {
+                    for (int j = 0; j < 30; j++)
+                    {
+                        if (bitmap.GetPixel(i, j).R == 39 && bitmap.GetPixel(i, j).G == 50 && bitmap.GetPixel(i, j).B == 39)
+                        {
+                            return false;
+                        }
+                        if(bitmap.GetPixel(i, j).R == 29 && bitmap.GetPixel(i, j).G == 31 && bitmap.GetPixel(i, j).B == 23)
+                        {
+                            return false;
+                        }
+                        if (bitmap.GetPixel(i, j).R == 23 && bitmap.GetPixel(i, j).G == 31 && bitmap.GetPixel(i, j).B == 23)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void setCoords(int x, int y)
+        {
+
+            gameScreenCoords[0] = x;
+            gameScreenCoords[1] = y;
+            player.setCoords(x, y);
+            interfaces.setCoords(x, y);
+            peripherals.setCoords(x, y);
+        }
+    }
+}
